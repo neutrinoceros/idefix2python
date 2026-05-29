e8822f39467d61e49674055c6738d8cf8fd4b510
David Fang <david.fang@ikmail.com>
2026-05-29 11:29:37 +0100

    remove hard coded

--- idefix2python/context.py
+++ idefix2python/context.py
@@ -389,7 +389,6 @@ class GridInfo:
 
                 if not zoom:
                     self.mask = np.full(self.grid1.shape, True, dtype=bool)
-                    self.mask = self.grid2 > 0
                     # )  # TODO hard coded, will be removed in later PR
                 else:
                     self.mask = (
